a7182a5e73df5d77c96df7e48d82d6bd5d3e1b3e
diehard5566 <shihyao001@gmail.com>
2022-04-16 00:56:47 +0800

    fix

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const https = require('https')
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000
+const TOKEN = process.env.LINE_ACCESS_TOKEN
 
-//middleware
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+)
 
-//route
 app.get('/', (req, res) => {
     res.sendStatus(200)
 })
 
-app.post('/webhook', (req, res) => {
+app.post('/webhook', function (req, res) {
     res.send('HTTP POST request sent to the webhook URL!')
-    //If the user sends a message to your bot, send a reply message
-    if (req.body.enents[0].type === 'message') {
-        //Message data, must be stringified
+    // If the user sends a message to your bot, send a reply message
+    if (req.body.events[0].type === 'message') {
+        // Message data, must be stringified
         const dataString = JSON.stringify({
-            replyToken: req.body.enents[0].replyToken,
+            replyToken: req.body.events[0].replyToken,
             messages: [
                 {
                     type: 'text',
@@ -30,13 +34,13 @@ app.post('/webhook', (req, res) => {
             ],
         })
 
-        //Request header
+        // Request header
         const headers = {
             'Content-Type': 'application/json',
             Authorization: 'Bearer ' + TOKEN,
         }
 
-        //Options to pass into the request
+        // Options to pass into the request
         const webhookOptions = {
             hostname: 'api.line.me',
             path: '/v2/bot/message/reply',
@@ -44,12 +48,25 @@ app.post('/webhook', (req, res) => {
             headers: headers,
             body: dataString,
         }
+
+        // Define request
+        const request = https.request(webhookOptions, res => {
+            res.on('data', d => {
+                process.stdout.write(d)
+            })
+        })
+
+        // Handle error
+        request.on('error', err => {
+            console.error(err)
+        })
+
+        // Send data
+        request.write(dataString)
+        request.end()
     }
 })
 
-const PORT = process.env.PORT || 3000
-const TOKEN = process.env.LINE_ACCESS_TOKEN
-
 app.listen(PORT, () => {
-    console.log(`poe-bot listening at http://localhost:${PORT}`)
+    console.log(`Example app listening at http://localhost:${PORT}`)
 })
