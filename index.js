dd7a46f21c153cfa6938576fda37d3bb3b0d6ea5
diehard5566 <shihyao001@gmail.com>
2022-04-16 16:02:53 +0800

    test change

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 app.post('/webhook', function (req, res) {
     res.send('HTTP POST request sent to the webhook URL!')
     // If the user sends a message to your bot, send a reply message
-    if (req.body.events[0].type === 'message') {
+    if (req.body.events[0].type === 'message' && req.body.events[0].message.text === '你是誰') {
         // Message data, must be stringified
         const dataString = JSON.stringify({
             replyToken: req.body.events[0].replyToken,
