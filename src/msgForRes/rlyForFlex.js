c4ef828f51a7532c756b7685f7bfcab7b2718f4a
diehard5566 <shihyao001@gmail.com>
2022-04-26 11:10:07 +0800

    test flex msg

--- src/msgForRes/rlyForFlex.js
+++ src/msgForRes/rlyForFlex.js
@@ -5,7 +5,7 @@ const currencyData = require('../currency.json')
 const replyFlexMsg = reqBody => {
     return JSON.stringify({
         replyToken: reqBody.events[0].replyToken,
-        messages: [{ type: 'flex', altText: 'Flex msg test', contents: currencyData, currencyData, currencyData }],
+        messages: [{ type: 'carousel', altText: 'Flex msg test', contents: currencyData, currencyData, currencyData }],
     })
 }
 
