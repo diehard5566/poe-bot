d6268ae10e34a95395f552091ac3c3d76e0b1655
diehard5566 <shihyao001@gmail.com>
2022-04-26 14:16:44 +0800

    fix flexMsg

--- src/msgForRes/rlyForFlex.js
+++ src/msgForRes/rlyForFlex.js
@@ -5,155 +5,159 @@ const replyFlexMsg = reqBody => {
         replyToken: reqBody.events[0].replyToken,
         messages: [
             {
-                type: 'carousel',
-                contents: [
-                    {
-                        type: 'bubble',
-                        hero: {
-                            type: 'image',
-                            size: 'full',
-                            aspectRatio: '20:13',
-                            aspectMode: 'cover',
-                            url: 'https://i.imgur.com/YeTZPiE.jpeg',
-                            action: {
-                                type: 'uri',
-                                label: 'action',
-                                uri: 'https://poe.ninja/challenge/currency/exalted-orb',
+                type: 'flex',
+                altText: 'this is a flex message',
+                contents: {
+                    type: 'carousel',
+                    contents: [
+                        {
+                            type: 'bubble',
+                            hero: {
+                                type: 'image',
+                                size: 'full',
+                                aspectRatio: '20:13',
+                                aspectMode: 'cover',
+                                url: 'https://i.imgur.com/YeTZPiE.jpeg',
+                                action: {
+                                    type: 'uri',
+                                    label: 'action',
+                                    uri: 'https://poe.ninja/challenge/currency/exalted-orb',
+                                },
+                            },
+                            body: {
+                                type: 'box',
+                                layout: 'vertical',
+                                spacing: 'sm',
+                                contents: [
+                                    {
+                                        type: 'text',
+                                        text: 'Exalted Orb',
+                                        wrap: true,
+                                        weight: 'bold',
+                                        size: 'xl',
+                                        color: '#ffffff',
+                                    },
+                                    {
+                                        type: 'text',
+                                        text: 'Buy:',
+                                        weight: 'bold',
+                                        style: 'normal',
+                                        margin: 'lg',
+                                        size: 'lg',
+                                        color: '#ffffff',
+                                    },
+                                    {
+                                        type: 'text',
+                                        text: 'Sell:',
+                                        weight: 'bold',
+                                        size: 'lg',
+                                        margin: 'lg',
+                                        color: '#ffffff',
+                                    },
+                                ],
+                                backgroundColor: '#464F69',
+                            },
+                            footer: {
+                                type: 'box',
+                                layout: 'vertical',
+                                spacing: 'sm',
+                                contents: [
+                                    {
+                                        type: 'button',
+                                        style: 'primary',
+                                        action: {
+                                            type: 'uri',
+                                            label: 'Buy(Poe.trade)',
+                                            uri: 'https://www.pathofexile.com/trade/exchange/Archnemesis/Nn8Vt0',
+                                        },
+                                    },
+                                    {
+                                        type: 'button',
+                                        action: {
+                                            type: 'uri',
+                                            label: 'Sell(Poe.trade)',
+                                            uri: 'https://www.pathofexile.com/trade/exchange/Archnemesis/12R5ck',
+                                        },
+                                        style: 'primary',
+                                    },
+                                ],
                             },
                         },
-                        body: {
-                            type: 'box',
-                            layout: 'vertical',
-                            spacing: 'sm',
-                            contents: [
-                                {
-                                    type: 'text',
-                                    text: 'Exalted Orb',
-                                    wrap: true,
-                                    weight: 'bold',
-                                    size: 'xl',
-                                    color: '#ffffff',
+                        {
+                            type: 'bubble',
+                            hero: {
+                                type: 'image',
+                                size: 'full',
+                                aspectRatio: '20:13',
+                                aspectMode: 'cover',
+                                url: 'https://i.imgur.com/YeTZPiE.jpeg',
+                                action: {
+                                    type: 'uri',
+                                    label: 'action',
+                                    uri: 'https://poe.ninja/challenge/currency/exalted-orb',
                                 },
-                                {
-                                    type: 'text',
-                                    text: 'Buy:',
-                                    weight: 'bold',
-                                    style: 'normal',
-                                    margin: 'lg',
-                                    size: 'lg',
-                                    color: '#ffffff',
-                                },
-                                {
-                                    type: 'text',
-                                    text: 'Sell:',
-                                    weight: 'bold',
-                                    size: 'lg',
-                                    margin: 'lg',
-                                    color: '#ffffff',
-                                },
-                            ],
-                            backgroundColor: '#464F69',
-                        },
-                        footer: {
-                            type: 'box',
-                            layout: 'vertical',
-                            spacing: 'sm',
-                            contents: [
-                                {
-                                    type: 'button',
-                                    style: 'primary',
-                                    action: {
-                                        type: 'uri',
-                                        label: 'Buy(Poe.trade)',
-                                        uri: 'https://www.pathofexile.com/trade/exchange/Archnemesis/Nn8Vt0',
+                            },
+                            body: {
+                                type: 'box',
+                                layout: 'vertical',
+                                spacing: 'sm',
+                                contents: [
+                                    {
+                                        type: 'text',
+                                        text: 'Exalted Orb',
+                                        wrap: true,
+                                        weight: 'bold',
+                                        size: 'xl',
+                                        color: '#ffffff',
                                     },
-                                },
-                                {
-                                    type: 'button',
-                                    action: {
-                                        type: 'uri',
-                                        label: 'Sell(Poe.trade)',
-                                        uri: 'https://www.pathofexile.com/trade/exchange/Archnemesis/12R5ck',
+                                    {
+                                        type: 'text',
+                                        text: 'Buy:',
+                                        weight: 'bold',
+                                        style: 'normal',
+                                        margin: 'lg',
+                                        size: 'lg',
+                                        color: '#ffffff',
                                     },
-                                    style: 'primary',
-                                },
-                            ],
-                        },
-                    },
-                    {
-                        type: 'bubble',
-                        hero: {
-                            type: 'image',
-                            size: 'full',
-                            aspectRatio: '20:13',
-                            aspectMode: 'cover',
-                            url: 'https://i.imgur.com/YeTZPiE.jpeg',
-                            action: {
-                                type: 'uri',
-                                label: 'action',
-                                uri: 'https://poe.ninja/challenge/currency/exalted-orb',
+                                    {
+                                        type: 'text',
+                                        text: 'Sell:',
+                                        weight: 'bold',
+                                        size: 'lg',
+                                        margin: 'lg',
+                                        color: '#ffffff',
+                                    },
+                                ],
+                                backgroundColor: '#464F69',
+                            },
+                            footer: {
+                                type: 'box',
+                                layout: 'vertical',
+                                spacing: 'sm',
+                                contents: [
+                                    {
+                                        type: 'button',
+                                        style: 'primary',
+                                        action: {
+                                            type: 'uri',
+                                            label: 'Buy(Poe.trade)',
+                                            uri: 'https://www.pathofexile.com/trade/exchange/Archnemesis/Nn8Vt0',
+                                        },
+                                    },
+                                    {
+                                        type: 'button',
+                                        action: {
+                                            type: 'uri',
+                                            label: 'Sell(Poe.trade)',
+                                            uri: 'https://www.pathofexile.com/trade/exchange/Archnemesis/12R5ck',
+                                        },
+                                        style: 'primary',
+                                    },
+                                ],
                             },
                         },
-                        body: {
-                            type: 'box',
-                            layout: 'vertical',
-                            spacing: 'sm',
-                            contents: [
-                                {
-                                    type: 'text',
-                                    text: 'Exalted Orb',
-                                    wrap: true,
-                                    weight: 'bold',
-                                    size: 'xl',
-                                    color: '#ffffff',
-                                },
-                                {
-                                    type: 'text',
-                                    text: 'Buy:',
-                                    weight: 'bold',
-                                    style: 'normal',
-                                    margin: 'lg',
-                                    size: 'lg',
-                                    color: '#ffffff',
-                                },
-                                {
-                                    type: 'text',
-                                    text: 'Sell:',
-                                    weight: 'bold',
-                                    size: 'lg',
-                                    margin: 'lg',
-                                    color: '#ffffff',
-                                },
-                            ],
-                            backgroundColor: '#464F69',
-                        },
-                        footer: {
-                            type: 'box',
-                            layout: 'vertical',
-                            spacing: 'sm',
-                            contents: [
-                                {
-                                    type: 'button',
-                                    style: 'primary',
-                                    action: {
-                                        type: 'uri',
-                                        label: 'Buy(Poe.trade)',
-                                        uri: 'https://www.pathofexile.com/trade/exchange/Archnemesis/Nn8Vt0',
-                                    },
-                                },
-                                {
-                                    type: 'button',
-                                    action: {
-                                        type: 'uri',
-                                        label: 'Sell(Poe.trade)',
-                                        uri: 'https://www.pathofexile.com/trade/exchange/Archnemesis/12R5ck',
-                                    },
-                                    style: 'primary',
-                                },
-                            ],
-                        },
-                    },
-                ],
+                    ],
+                },
             },
         ],
     })
