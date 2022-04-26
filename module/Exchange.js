dac6e6b5d8f3d8e12bae43c5149a52a6d50a879b
diehard5566 <shihyao001@gmail.com>
2022-04-27 00:16:24 +0800

    add more currency

--- module/Exchange.js
+++ module/Exchange.js
@@ -13,13 +13,22 @@ const getExchange = async () => {
     }
 
     //ex
-    const res = await fetch(ninjaAPI + '2', ninjaOption)
-    const exaltedOrb = await res.json()
+    const res1 = await fetch(ninjaAPI + '2', ninjaOption)
+    const exaltedOrb = await res1.json()
     const currentEx = Math.round(
         exaltedOrb.receiveCurrencyGraphData[exaltedOrb.receiveCurrencyGraphData.length - 1].value
     )
     exchangeInfo.set('currentEx', currentEx)
     const todayExPrice = exchangeInfo.get('currentEx')
+
+    //mirror
+    const res0 = await fetch(ninjaAPI + '22', ninjaOption)
+    const mirror = await res0.json()
+    const currentMirror = Math.round(
+        mirror.receiveCurrencyGraphData[mirror.receiveCurrencyGraphData.length - 1].value / currentEx
+    )
+    exchangeInfo.set('currentMirror', currentMirror)
+    const todayMirrorPrice = exchangeInfo.get('currentMirror')
 
     //c
     const chaos = 1
@@ -27,15 +36,66 @@ const getExchange = async () => {
     //alt
     const res2 = await fetch(ninjaAPI + '6', ninjaOption)
     const alt = await res2.json()
-    const altNoExchang =
+    const altExchange =
         Math.round(alt.receiveCurrencyGraphData[alt.receiveCurrencyGraphData.length - 1].value * 100) / 100
-    const currentAlt = Math.round(+(chaos / altNoExchang).toFixed(3)) * currentEx
+    const currentAlt = Math.round(+(chaos / altExchange).toFixed(3)) * currentEx
     exchangeInfo.set('currentAlt', currentAlt)
     const todayAltPrice = exchangeInfo.get('currentAlt')
 
+    //scouring
+    const res3 = await fetch(ninjaAPI + '14', ninjaOption)
+    const scouring = await res3.json()
+    const scoExchange =
+        Math.round(scouring.receiveCurrencyGraphData[scouring.receiveCurrencyGraphData.length - 1].value * 100) / 100
+    const currentSco = Math.round(+(chaos / scoExchange).toFixed(3)) * currentEx
+    exchangeInfo.set('currentSco', currentSco)
+    const todayScoPrice = exchangeInfo.get('currentSco')
+
+    //fusing
+    const res4 = await fetch(ninjaAPI + '5', ninjaOption)
+    const fusing = await res4.json()
+    const fusExchange =
+        Math.round(fusing.receiveCurrencyGraphData[fusing.receiveCurrencyGraphData.length - 1].value * 100) / 100
+    const currentFus = +(chaos / fusExchange).toFixed(1) * currentEx
+    exchangeInfo.set('currentFus', currentFus)
+    const todayFusPrice = exchangeInfo.get('currentFus')
+
+    //regret
+    const res5 = await fetch(ninjaAPI + '9', ninjaOption)
+    const regret = await res5.json()
+    const regExchange =
+        Math.round(regret.receiveCurrencyGraphData[regret.receiveCurrencyGraphData.length - 1].value * 100) / 100
+    const currentReg = +(chaos / regExchange).toFixed(2)
+    exchangeInfo.set('currentReg', currentReg)
+    const todayRegPrice = exchangeInfo.get('currentReg')
+
+    //vaal orb
+    const res6 = await fetch(ninjaAPI + '8', ninjaOption)
+    const vaalOrb = await res6.json()
+    const vaalExchange =
+        Math.round(vaalOrb.receiveCurrencyGraphData[vaalOrb.receiveCurrencyGraphData.length - 1].value * 100) / 100
+    const currentVaal = +(chaos / vaalExchange).toFixed(1) * currentEx
+    exchangeInfo.set('currentVaal', currentVaal)
+    const todayVaalPrice = exchangeInfo.get('currentVaal')
+
+    //alc
+    const res7 = await fetch(ninjaAPI + '4', ninjaOption)
+    const alc = await res7.json()
+    const alcExchange =
+        Math.round(alc.receiveCurrencyGraphData[alc.receiveCurrencyGraphData.length - 1].value * 100) / 100
+    const currentAlc = +(chaos / alcExchange).toFixed(3)
+    exchangeInfo.set('currentAlc', currentAlc)
+    const todayAlcPrice = exchangeInfo.get('currentAlc')
+
     return {
         Ex: todayExPrice,
+        Mirror: todayMirrorPrice,
         Alt: todayAltPrice,
+        Sco: todayScoPrice,
+        Fus: todayFusPrice,
+        Reg: todayRegPrice,
+        Vaal: todayVaalPrice,
+        Alc: todayAlcPrice,
     }
 }
 
--- src/msgForRes/rlyForFlex.js
+++ src/msgForRes/rlyForFlex.js
@@ -42,18 +42,21 @@ const getExchange = require('../../module/Exchange')
 
 const replyFlexMsg = async reqBody => {
     const currentPrice = await getExchange()
-    console.log('currentPrice:', currentPrice.Ex)
+    // console.log('currentPrice:', currentPrice.Ex)
 
-    currencyData.contents.contents[0].body.contents[1].text = `現在1崇高約為${currentPrice.Ex}c`
-    currencyData.contents.contents[1].body.contents[1].text = `現在改造約為${currentPrice.Alt}崇高`
+    currencyData.contents.contents[0].body.contents[1].text = `現在鏡子約為${currentPrice.Mirror}個崇高`
+    currencyData.contents.contents[1].body.contents[1].text = `現在1崇高約能買${currentPrice.Ex}個混沌`
+    currencyData.contents.contents[2].body.contents[1].text = `現在1崇高約能買${currentPrice.Alt}個改造`
+    currencyData.contents.contents[3].body.contents[1].text = `現在1崇高約能買${currentPrice.Sco}個重鑄`
+    currencyData.contents.contents[4].body.contents[1].text = `現在1崇高約能買${currentPrice.Fus}個連結`
+    currencyData.contents.contents[5].body.contents[1].text = `現在1混沌約能買${currentPrice.Reg}個後悔`
+    currencyData.contents.contents[6].body.contents[1].text = `現在1崇高約能買${currentPrice.Vaal}個瓦寶`
+    currencyData.contents.contents[7].body.contents[1].text = `現在1混沌約能買${currentPrice.Alc}個點金`
 
     const sendData = JSON.stringify({
         replyToken: reqBody.events[0].replyToken,
         messages: [currencyData],
     })
-
-    console.log(currencyData.contents.contents[0].body.contents[1].text)
-    console.log(currencyData.contents.contents[1].body.contents[1].text)
 
     return sendData
 }
