be5bdff9cfbd2e49788e0a6caf7a17f9ccbdbe9a
diehard5566 <shihyao001@gmail.com>
2022-04-27 10:04:41 +0800

    fix default msg

--- src/msgForRes/rlpForDefault.js
+++ src/msgForRes/rlpForDefault.js
@@ -4,6 +4,10 @@ const replyDefaultMsg = reqBody => {
         messages: [
             {
                 type: 'text',
+                text: '您好啊！流亡者',
+            },
+            {
+                type: 'text',
                 text: '請輸入：指令',
             },
         ],
@@ -16,14 +20,7 @@ const replyForCommand = reqBody => {
         messages: [
             {
                 type: 'text',
-                text:
-                    '指令：' +
-                    '\n' +
-                    '1.帳號(查詢帳號角色/裝備)' +
-                    '\n' +
-                    '2.裝備(查詢所有裝備官方賣場)' +
-                    '\n' +
-                    '3.通貨(查詢通貨價格 from ninja)',
+                text: '指令：' + '\n' + '1.帳號(查詢帳號角色/裝備)' + '\n' + '2.通貨(查詢通貨價格 from ninja) ' + '\n',
             },
         ],
     })
