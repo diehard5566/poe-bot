58179378e931b3870422fa93af81c7b2a15fa5a4
diehard5566 <shihyao001@gmail.com>
2022-05-03 22:32:49 +0800

    fix password

--- db/connect.js
+++ db/connect.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
-    //password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD,
     waitForConnections: true,
     connectionLimit: 10, // 最大連線數
     queueLimit: 0,
