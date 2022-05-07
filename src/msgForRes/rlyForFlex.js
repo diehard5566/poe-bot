7f4032ecbaf295a1825a4ea633eeef54a52ec731
diehard5566 <shihyao001@gmail.com>
2022-05-07 19:04:36 +0800

    fixed message

--- src/msgForRes/rlyForFlex.js
+++ src/msgForRes/rlyForFlex.js
@@ -1,6 +1,6 @@
 const currencyData = require('../currency.json')
 const getExchange = require('../../module/Exchange')
-const postFromPoedb = require('../newPostFromPoedb.json')
+// const postFromPoedb = require('../newPostFromPoedb.json')
 // const reqBody = {
 //     destination: 'xxxxxxxxxx',
 //     events: [
@@ -63,18 +63,61 @@ const replyFlexMsg = async reqBody => {
 }
 
 const pushMsg = recent => {
+    const postFromPoedb = {
+        type: 'flex',
+        altText: '來自編年史的最新消息',
+        contents: {
+            type: 'carousel',
+            contents: [
+                {
+                    type: 'bubble',
+                    hero: {
+                        type: 'image',
+                        url: 'https://scontent.ftpe7-3.fna.fbcdn.net/v/t31.18172-8/11116573_1431516947156679_2592834439141048769_o.jpg?_nc_cat=108&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=q9yd5vTENkQAX9f0olG&_nc_ht=scontent.ftpe7-3.fna&oh=00_AT-Cfm9i3A-q8UdOH1coxjZJIbg4WpOf1ouxJ0z9n7mXMA&oe=629C82C6',
+                        size: 'full',
+                        aspectRatio: '20:13',
+                        aspectMode: 'cover',
+                        action: {
+                            type: 'uri',
+                            uri: 'https://www.facebook.com/poedbtw',
+                        },
+                    },
+                    body: {
+                        type: 'box',
+                        layout: 'vertical',
+                        contents: [
+                            {
+                                type: 'text',
+                                text: '最新消息：',
+                            },
+                            {
+                                type: 'text',
+                                text: 'default',
+                                weight: 'bold',
+                                size: 'xl',
+                                align: 'center',
+                            },
+                        ],
+                    },
+                },
+            ],
+        },
+    }
+
     const post_text = recent.post_text
     const post_img = recent.post_img
 
     if (post_img != null) {
-        postFromPoedb.hero.url = post_img
+        postFromPoedb.contents.contents[0].hero.url = post_img
     }
 
     postFromPoedb.contents.contents[0].body.contents[1].text = post_text
 
-    const message = postFromPoedb
+    // const msg = {
+    //     messages: [postFromPoedb],
+    // }
 
-    return message
+    return postFromPoedb
 }
 
 module.exports = {
